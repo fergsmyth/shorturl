95c415244d23089755505c9ef547642a5258a73a
Ferg Smyth <fergsmyth@gmail.com>
2019-10-13 19:18:55 +0100

    Adding cache to getBaseUrl

--- src/main/java/com/ferg/url/URLRepository.java
+++ src/main/java/com/ferg/url/URLRepository.java
@@ -1,9 +1,15 @@
 package com.ferg.url;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface URLRepository extends CrudRepository<URLEntity, Long> {
+
+    @Cacheable("urls")
+    Optional<URLEntity> getURLByBase64(String base64);
 
 }
--- src/main/java/com/ferg/url/URLService.java
+++ src/main/java/com/ferg/url/URLService.java
@@ -13,7 +13,9 @@ public class URLService {
 
     public String shortenUrl(String longURL){
         String base64 = Base64.getEncoder().withoutPadding().encodeToString(longURL.getBytes());
-        urlRepository.save(new URLEntity(base64, longURL));
+        if(!urlRepository.getURLByBase64(base64).isPresent()){
+            urlRepository.save(new URLEntity(base64, longURL));
+        }
         return base64;
     }
 }
