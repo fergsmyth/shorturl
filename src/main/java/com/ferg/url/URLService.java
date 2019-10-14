7bbd60b8dfa70c48c232aaf5593226569d7fe4c3
Ferg Smyth <fergsmyth@gmail.com>
2019-10-14 15:55:00 +0100

    Adding interview potential questions

--- src/main/java/com/ferg/url/URLService.java
+++ src/main/java/com/ferg/url/URLService.java
@@ -3,6 +3,8 @@ package com.ferg.url;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 @Service
@@ -11,7 +13,8 @@ public class URLService {
     @Autowired
     private URLRepository urlRepository;
 
-    public String shortenUrl(String longURL){
+    public String shortenUrl(String longURL) throws NoSuchAlgorithmException {
+        MessageDigest.getInstance("MD5").update(longURL.getBytes());
         String base64 = Base64.getEncoder().withoutPadding().encodeToString(longURL.getBytes());
         if(!urlRepository.getURLByBase64(base64).isPresent()){
             urlRepository.save(new URLEntity(base64, longURL));
